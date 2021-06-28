d8f47661d5bb99415ec43a10630ad5b94861840b
Ingley <michael.ingley@gmail.com>
2021-06-28 09:38:39 -0700

    adds comments

--- leetcode/src/problems/_94_bst_inorder_traversal.rs
+++ leetcode/src/problems/_94_bst_inorder_traversal.rs
@@ -24,18 +24,25 @@ use std::rc::Rc;
 use std::cell::RefCell;
 impl Solution {
     pub fn inorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
-        // response vector, on each visited node, push value found here
+        // response vector, on each visited node, values discovered in traversal will be pushed to here
         let mut res: Vec<i32> = Vec::new();
+        // stack to track nodes we still have to visit
         let mut stack = Vec::new();
+        // working copy of tree passed into function
         let mut r = root.clone();
+        // while working copy of tree still has nodes left to traverse
         while r.is_some() || !stack.is_empty() {
+            // if current iteration yields a node, push it to the stack, go left for inorder traversal
             while let Some(node) = r {
                 stack.push(node.clone());
                 r = node.borrow().left.clone();
             }
+            // once we run of out left children nodes to visit, start working on the stack we just created.
             r = stack.pop();
+            // will start with deepest left child, and collect the value of the node by pushing to response vec
             if let Some(node) = r {
                 res.push(node.borrow().val);
+                // since we are on deepest left node, now it's okay to check for right children, update r and continue traversal
                 r = node.borrow().right.clone();
             }
         }
---
+++ leetcode/target/debug/deps/leetcode-99d09cfc4cfc84fc.d
@@ -0,0 +1,7 @@
+/Users/mingley/Work/rust/gym/leetcode/target/debug/deps/leetcode-99d09cfc4cfc84fc.rmeta: src/lib.rs src/problems.rs src/problems/_94_bst_inorder_traversal.rs
+
+/Users/mingley/Work/rust/gym/leetcode/target/debug/deps/leetcode-99d09cfc4cfc84fc.d: src/lib.rs src/problems.rs src/problems/_94_bst_inorder_traversal.rs
+
+src/lib.rs:
+src/problems.rs:
+src/problems/_94_bst_inorder_traversal.rs:
