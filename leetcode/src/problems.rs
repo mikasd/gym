c7d6279a132fe800e60feb20675fc09251353e9d
Ingley <michael.ingley@gmail.com>
2021-06-29 05:38:16 -0700

    108

--- leetcode/src/problems.rs
+++ leetcode/src/problems.rs
@@ -2,3 +2,4 @@ mod _94_bst_inorder_traversal;
 mod _5_longest_palindromic_substring_manachers_algorithm_impl;
 mod _19_remove_nth_node_from_list;
 mod _15_3sum;
+mod _108_convert_sorted_arr_to_bst;
--- leetcode/src/problems/_15_3sum.rs
+++ leetcode/src/problems/_15_3sum.rs
@@ -3,39 +3,47 @@ struct Solution;
 impl Solution {
     pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
         let mut ret = Vec::new();
-        
-        if nums.is_empty() { return ret; }
-        if nums.len() == 1 { return ret; }
-        
+
+        if nums.is_empty() {
+            return ret;
+        }
+        if nums.len() == 1 {
+            return ret;
+        }
+
         let mut nums = nums;
         nums.sort();
-        
-        for i in 0..(nums.len()-2) {
-            if i > 0 && nums[i] == nums[i-1] {continue}
 
-            if nums[i] > 0 { return ret; }
-            
+        for i in 0..(nums.len() - 2) {
+            if i > 0 && nums[i] == nums[i - 1] {
+                continue;
+            }
+
+            if nums[i] > 0 {
+                return ret;
+            }
+
             let mut left = i + 1;
             let mut right = nums.len() - 1;
-            
+
             while left < right {
-                let L = nums[left];
-                let R = nums[right];
-                let X = nums[i];
-                let total = L + R + X;
+                let l = nums[left];
+                let r = nums[right];
+                let x = nums[i];
+                let total = l + r + x;
                 if total > 0 {
                     right -= 1;
                 } else if total < 0 {
                     left += 1;
                 } else {
-                    ret.push(vec![L, R, X]);
-                    while left < right && nums[left] == nums[left+1] {
+                    ret.push(vec![l, r, x]);
+                    while left < right && nums[left] == nums[left + 1] {
                         left += 1;
                     }
-                    while left < right && nums[right] == nums[right-1] {
+                    while left < right && nums[right] == nums[right - 1] {
                         right -= 1;
                     }
-                    
+
                     left += 1;
                     right -= 1;
                 }
